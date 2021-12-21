f67d7d8e67ce57b6ac6e24fe454fb11301f1dd5e
IshanRajeshGupta <you@example.com>
2021-12-21 17:38:02 +0530

    first commit

---
+++ precache-manifest.b002b839dc61e6d06fa3da55ec7323b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bfd44285f0b907370d6",
+    "url": "./static/css/main.636f961f.chunk.css"
+  },
+  {
+    "revision": "8bfd44285f0b907370d6",
+    "url": "./static/js/main.8bfd4428.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "./static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0c8ab6f31753a524950c5e20064c6d0d",
+    "url": "./index.html"
+  }
+];
